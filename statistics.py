3071cb063709dd1a0c16fb5000cad29e80673da8
zhangce <czhang@cs.stanford.edu>
2022-08-19 19:32:50 -0500

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,1 +1,11 @@
 
+import subprocess
+
+MODE = "lsf"
+
+if MODE == "lsf":
+
+  proc = subprocess.Popen(["lsload", "-gpuload", "-w"], stdout=subprocess.PIPE, shell=True)
+  (out, err) = proc.communicate()
+  
+  print(out)

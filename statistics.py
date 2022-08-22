2fc1da266a61709dd7bf48e09af807a0e799408d
Binhang Yuan <biyuan@sc.stanford.edu>
2022-08-21 20:55:13 -0700

    slurm

--- statistics.py
+++ statistics.py
@@ -6,6 +6,31 @@ MODE = "slurm"
 machines = {}
 total_gpus = 0
 
+devices = {}
+
+# {u'titanxp': 1, u'titanrtx': 1, u'a5000': 1, u'3090': 1, u'titanx:2,gpu:titanxp': 1, u'titanx:1,gpu:2080ti': 1, u'titanx': 1, u'titanv': 1}
+
+
+device_map = {
+    "titanxp" : "NVIDIA TITAN Xp",
+    "titanrtx" : "NVIDIA TITAN RTX" ,
+    "a5000" : "NVIDIA RTX A5000"   ,
+    "3090" : "NVIDIA GeForce RTX 3090"         ,
+    "2080ti": "NVIDIA GeForce RTX 2080 Ti"        ,
+    "titanx": "NVIDIA GeForce GTX TITAN X"       ,
+    "titanv": "NVIDIA TITAN V"
+}
+
+# (FP32 TFLOPS, TENSOR TFLOPS, MEMORY GB, BANDWIDTH GB/S, SOURCE)
+devices = {
+    "NVIDIA TITAN Xp" : (12.15, 12.15, 12, 547.6, "https://www.techpowerup.com/gpu-specs/titan-xp.c2948"),
+    "NVIDIA TITAN RTX" : (16.31, 32.62, 24, 672, "https://www.techpowerup.com/gpu-specs/titan-rtx.c3311"),
+    "NVIDIA RTX A5000" : (24.78, 222.2, 24, 768, "https://www.leadtek.com/eng/products/workstation_graphics(2)/NVIDIA_RTX_A5000(40914)/detail"),
+    "NVIDIA GeForce RTX 3090": (35.58, 285, 24, 936, "https://wccftech.com/roundup/nvidia-geforce-rtx-3090-ti/"),
+    "NVIDIA GeForce RTX 2080 Ti": (13.4, 114, 11, 616, "https://mightygadget.co.uk/nvidia-geforce-rtx-3080-vs-rtx-2080-ti/"),
+    "NVIDIA GeForce GTX TITAN X": (6.691, 6.691, 12, 336.5, "https://www.nvidia.com/en-us/geforce/graphics-cards/geforce-gtx-titan-x/specifications/"),
+    "NVIDIA TITAN V": (14.90, 29.80, 12, 651.3, "https://www.techpowerup.com/gpu-specs/titan-v.c3051")
+}
 
 if MODE == "slurm":
 
@@ -50,8 +75,7 @@ if MODE == "slurm":
                 machines[hostname]["n_gpu"] = ngpus
                 machines[hostname]["avail"] = ngpus - nalloc
 
-            
-            total_gpus = total_gpus + (ngpus - nalloc)    
+            devices[devicename] = 1
 
 
 if MODE == "condor":
@@ -84,8 +108,9 @@ if MODE == "condor":
 
 			print(machine_name, devicename)
 
+                        devices[devicename] = 1
+                        
 #print("######")
-
 
 if MODE == "lsf":
 
@@ -108,6 +133,8 @@ if MODE == "lsf":
       machines[machine_name] = {}
       machines[machine_name]["gpu_model"] = gpu_model
       machines[machine_name]["n_gpu"] = 1
+
+      devices[gpu_model] = 1
 
     elif len(fields) == 12:
       machines[machine_name]["n_gpu"] = machines[machine_name]["n_gpu"] + 1
@@ -133,8 +160,20 @@ print ("Total GPUs", total_gpus)
 
 for machine_name in machines:
 
-  print(machine_name, machines[machine_name]["gpu_model"], machines[machine_name]["n_gpu"], machines[machine_name]["avail"])
+    device = machines[machine_name]["gpu_model"]
+    if device not in device_map:
+        print ("UNKNOWN DEVICES", device)
+        continue
 
+    (fp32, fp16, mem, band, source) = devices[device_map[device]]
+    
+    print(machine_name, machines[machine_name]["gpu_model"], machines[machine_name]["n_gpu"], machines[machine_name]["avail"], fp16)
+  
+
+
+
+
+"""
 import requests
 
 res = requests.post('https://planetd.shift.ml/site_stats', json={
@@ -145,5 +184,5 @@ res = requests.post('https://planetd.shift.ml/site_stats', json={
   "note": "string"
 })
 
-
-
+print devices
+"""
